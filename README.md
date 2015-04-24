148a9378b96819da864cc9f6d848f17f9d1ec191
Pierre <pierre@bootic.com>
2015-04-24 09:32:14 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Dancer2::Session::CGISession - Blah blah blah
+
+# SYNOPSIS
+
+    use Dancer2::Session::CGISession;
+
+# DESCRIPTION
+
+Dancer2::Session::CGISession is
+
+# AUTHOR
+
+Pierre VIGIER <pierer.vigier@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2015- Pierre VIGIER
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Dancer2/Session/CGISession.pm
@@ -0,0 +1,39 @@
+package Dancer2::Session::CGISession;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Dancer2::Session::CGISession - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Dancer2::Session::CGISession;
+
+=head1 DESCRIPTION
+
+Dancer2::Session::CGISession is
+
+=head1 AUTHOR
+
+Pierre VIGIER E<lt>pierer.vigier@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015- Pierre VIGIER
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
